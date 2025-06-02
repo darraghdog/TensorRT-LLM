dd71252c4d2376ce9e40583762f7e132ffb573bb
darraghdog <darragh.hanley@gmail.com>
2025-06-02 12:05:16 +0100

    .

--- examples/redrafter/convert_checkpoint_qwen.py
+++ examples/redrafter/convert_checkpoint_qwen.py
@@ -341,7 +341,7 @@ def convert_and_save(
     # print(type(hf_base_model))
     print(type(weights))
     print(type(drafter_weights))
-    weights_dict = "\n".join([f'{k} : {v.shape} {v.dtype} {type(v)}' for k,v in hf_base_model.named_parameters()])
+    weights_dict = "\n".join([f'{k} : {v.shape} {v.dtype} {type(v)}' for k,v in weights.items()])
     print(f'hf_base_model : {weights_dict}')
     drafter_weights_dict = "\n".join([f'{k} :{v.shape} {v.dtype} {type(v)}' for k,v in drafter_weights.items()])
     print(f'Drafter weights : {drafter_weights_dict}')
