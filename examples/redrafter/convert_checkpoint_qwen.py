498129724c0b54da829177f8c71582ac4d57aa94
darraghdog <darragh.hanley@gmail.com>
2025-06-02 17:39:54 +0100

    Convert bf16 to fp16

--- examples/redrafter/convert_checkpoint_qwen.py
+++ examples/redrafter/convert_checkpoint_qwen.py
@@ -303,6 +303,8 @@ def convert_and_save(
 
     model_config = PretrainedConfig.from_json_file(config_path)
     model_config = copy.deepcopy(model_config)
+    if model_config.dtype == "bfloat16":
+        model_config.dtype = "float16"
 
     # Prepare rank-specific configuration
     rank_config = copy.deepcopy(model_config)
@@ -312,11 +314,6 @@ def convert_and_save(
     weights_safe = safetensors.safe_open(stade_dict_path, framework="pt")
     weights = {k: weights_safe.get_tensor(k) for k in weights_safe.keys()}
 
-    # Convert bfloat16 tensors if needed
-    weights = {
-        k: v if not type(v) is torch.bfloat16 else v.to(torch.bfloat16)
-        for k, v in weights.items()
-    }
 
     if hf_drafter_model is not None:
         drafter_weights = hf_drafter(
